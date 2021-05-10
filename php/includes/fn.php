4ff8abb47ff12c43020ccb3ee45442e585037daf
Int2cs <gabriel.gn87@gmail.com>
2021-05-10 09:00:50 +0200

    ajout de strip_tags pour la vérification des inputs

--- php/includes/fn.php
+++ php/includes/fn.php
@@ -3,6 +3,7 @@ function inpValidate($donnees)
 {
   $donnees = trim($donnees);
   $donnees = stripslashes($donnees);
+  $donnees = strip_tags($donnees);
   $donnees = htmlspecialchars($donnees);
   return $donnees;
 }
